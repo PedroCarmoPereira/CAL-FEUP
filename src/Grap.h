5c6f15f2870abb55d079c7a976074d30c5b45eca
pedrocarmopereira <pppereira3@gmail.com>
2019-05-20 23:27:44 +0100

    MAPS

---
+++ src/Grap.h
@@ -0,0 +1,299 @@
+/*
+ * Graph.h
+ */
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <vector>
+#include <queue>
+#include <list>
+#include <limits>
+#include <cmath>
+
+using namespace std;
+
+template <class T> class Edge;
+template <class T> class Graph;
+template <class T> class Vertex;
+
+#define INF std::numeric_limits<double>::max()
+
+/************************* Vertex  **************************/
+
+template <class T>
+class Vertex {
+	T info;                // contents
+	vector<Edge<T> > adj;  // outgoing edges
+	bool visited;          // auxiliary field
+	double dist = 0;
+	Vertex<T> *path = NULL;
+
+	bool processing = false;
+	void addEdge(Vertex<T> *dest, double w);
+
+public:
+	Vertex(T in);
+	bool operator<(Vertex<T> & vertex) const; // // required by MutablePriorityQueue
+	T getInfo() const;
+	double getDist() const;
+	Vertex *getPath() const;
+	bool removeEdgeTo(Vertex<T> *d);
+	friend class Graph<T>;
+};
+
+
+template <class T>
+Vertex<T>::Vertex(T in): info(in), visited(false) {}
+
+/*
+ * Auxiliary function to add an outgoing edge to a vertex (this),
+ * with a given destination vertex (d) and edge weight (w).
+ */
+template <class T>
+void Vertex<T>::addEdge(Vertex<T> *d, double w) {
+	adj.push_back(Edge<T>(d, w));
+}
+
+template <class T>
+bool Vertex<T>::operator<(Vertex<T> & vertex) const {
+	return this->dist < vertex.dist;
+}
+
+template <class T>
+T Vertex<T>::getInfo() const {
+	return this->info;
+}
+
+template <class T>
+double Vertex<T>::getDist() const {
+	return this->dist;
+}
+
+template <class T>
+Vertex<T> *Vertex<T>::getPath() const {
+	return this->path;
+}
+
+/********************** Edge  ****************************/
+
+template <class T>
+class Edge {
+	Vertex<T> * dest;      // destination vertex
+	double weight;         // edge weight
+public:
+	Edge(Vertex<T> *d, double w);
+	friend class Graph<T>;
+	friend class Vertex<T>;
+};
+
+template <class T>
+Edge<T>::Edge(Vertex<T> *d, double w): dest(d), weight(w) {}
+
+
+/*************************** Graph  **************************/
+
+template <class T>
+class Graph {
+	vector<Vertex<T> *> vertexSet;    // vertex set
+
+public:
+	Vertex<T> *findVertex(const T &in) const;
+	bool addVertex(const T &in);
+	bool addEdge(const T &sourc, const T &dest, double w);
+	bool removeEdge(const T &sourc, const T &dest);
+	bool removeVertex(const T &in);
+	vector<T> dfs() const;
+	void dfsVisit(Vertex<T> *v, vector<T> & res) const;
+	vector<T> bfs(const T & source) const;
+	int getNumVertex() const;
+	vector<Vertex<T> *> getVertexSet() const;
+
+	void dijkstraShortestPath(const T &s);
+	void floydWarshallShortestPath();
+	vector<T> getPath(const T &origin, const T &dest) const;
+	vector<T> getfloydWarshallPath(const T &origin, const T &dest) const;
+
+};
+
+template <class T>
+int Graph<T>::getNumVertex() const {
+	return vertexSet.size();
+}
+
+template <class T>
+vector<Vertex<T> *> Graph<T>::getVertexSet() const {
+	return vertexSet;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+template <class T>
+Vertex<T> * Graph<T>::findVertex(const T &in) const {
+	for (auto v : vertexSet)
+		if (v->info == in)
+			return v;
+	return NULL;
+}
+
+/*
+ *  Adds a vertex with a given content or info (in) to a graph (this).
+ *  Returns true if successful, and false if a vertex with that content already exists.
+ */
+template <class T>
+bool Graph<T>::addVertex(const T &in) {
+	if ( findVertex(in) != NULL)
+		return false;
+	vertexSet.push_back(new Vertex<T>(in));
+	return true;
+}
+
+/*
+ * Adds an edge to a graph (this), given the contents of the source and
+ * destination vertices and the edge weight (w).
+ * Returns true if successful, and false if the source or destination vertex does not exist.
+ */
+template <class T>
+bool Graph<T>::addEdge(const T &sourc, const T &dest, double w) {
+	auto v1 = findVertex(sourc);
+	auto v2 = findVertex(dest);
+	if (v1 == NULL || v2 == NULL)
+		return false;
+	v1->addEdge(v2,w);
+	return true;
+}
+
+
+/*
+ * Removes an edge from a graph (this).
+ * The edge is identified by the source (sourc) and destination (dest) contents.
+ * Returns true if successful, and false if such edge does not exist.
+ */
+template <class T>
+bool Graph<T>::removeEdge(const T &sourc, const T &dest) {
+	auto v1 = findVertex(sourc);
+	auto v2 = findVertex(dest);
+	if (v1 == NULL || v2 == NULL)
+		return false;
+	return v1->removeEdgeTo(v2);
+}
+
+/*
+ * Auxiliary function to remove an outgoing edge (with a given destination (d))
+ * from a vertex (this).
+ * Returns true if successful, and false if such edge does not exist.
+ */
+template <class T>
+bool Vertex<T>::removeEdgeTo(Vertex<T> *d) {
+	for (auto it = adj.begin(); it != adj.end(); it++)
+		if (it->dest  == d) {
+			adj.erase(it);
+			return true;
+		}
+	return false;
+}
+
+
+/*
+ *  Removes a vertex with a given content (in) from a graph (this), and
+ *  all outgoing and incoming edges.
+ *  Returns true if successful, and false if such vertex does not exist.
+ */
+template <class T>
+bool Graph<T>::removeVertex(const T &in) {
+	for (auto it = vertexSet.begin(); it != vertexSet.end(); it++)
+		if ((*it)->info  == in) {
+			auto v = *it;
+			vertexSet.erase(it);
+			for (auto u : vertexSet)
+				u->removeEdgeTo(v);
+			delete v;
+			return true;
+		}
+	return false;
+}
+
+
+
+/*
+ * Performs a depth-first search (dfs) in a graph (this).
+ * Returns a vector with the contents of the vertices by dfs order.
+ * Follows the algorithm described in theoretical classes.
+ */
+template <class T>
+vector<T> Graph<T>::dfs() const {
+	vector<T> res;
+	for (auto v : vertexSet)
+		v->visited = false;
+	for (auto v : vertexSet)
+	    if (! v->visited)
+	    	dfsVisit(v, res);
+	return res;
+}
+
+/*
+ * Auxiliary function that visits a vertex (v) and its adjacent, recursively.
+ * Updates a parameter with the list of visited node contents.
+ */
+template <class T>
+void Graph<T>::dfsVisit(Vertex<T> *v, vector<T> & res) const {
+	v->visited = true;
+	res.push_back(v->info);
+	for (auto & e : v->adj) {
+		auto w = e.dest;
+	    if ( ! w->visited)
+	    	dfsVisit(w, res);
+	}
+}
+
+/*
+ * Performs a breadth-first search (bfs) in a graph (this), starting
+ * from the vertex with the given source contents (source).
+ * Returns a vector with the contents of the vertices by dfs order.
+ * Follows the algorithm described in theoretical classes.
+ */
+template <class T>
+vector<T> Graph<T>::bfs(const T & source) const {
+	vector<T> res;
+	auto s = findVertex(source);
+	if (s == NULL)
+		return res;
+	queue<Vertex<T> *> q;
+	for (auto v : vertexSet)
+		v->visited = false;
+	q.push(s);
+	s->visited = true;
+	while (!q.empty()) {
+		auto v = q.front();
+		q.pop();
+		res.push_back(v->info);
+		for (auto & e : v->adj) {
+			auto w = e.dest;
+		    if ( ! w->visited ) {
+				q.push(w);
+				w->visited = true;
+		    }
+		}
+	}
+	return res;
+}
+
+
+
+
+/**************** Single Source Shortest Path algorithms ************/
+
+template<class T>
+void Graph<T>::dijkstraShortestPath(const T &origin) {
+	// TODO
+}
+
+/**************** All Pairs Shortest Path  ***************/
+
+template<class T>
+void Graph<T>::floydWarshallShortestPath() {
+	// TODO
+}
+
+
+#endif /* GRAPH_H_ */
