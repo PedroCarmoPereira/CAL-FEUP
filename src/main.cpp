5a2b6cac89a5d5c25a6ea19f37ef0161fac10056
FilipeCarlosNogueira <fcadcn@gmail.com>
2019-05-21 20:36:21 +0100

    makefile

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(){
 	string nodeFile, edgeFile;
-	setFiles(FAFE, nodeFile, edgeFile);
+	setFiles(PORTO, nodeFile, edgeFile);
 	cout << nodeFile << endl;
 	cout << edgeFile << endl;
 
 	GraphViewer *gv = new GraphViewer(900, 1400, true);
-	gv->createWindow(600, 600);
+	gv->createWindow(900, 1400);
 	gv->defineVertexColor("blue");
 	gv->defineEdgeColor("black");
 	//gv->closeWindow();
