81cf86dd5eeed65d4407df6d2bba929fa71f05c5
artfs <artfs23@gmail.com>
2019-05-28 18:15:40 +0100

    FIx

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -1,16 +1,18 @@
 #include "Driver.h"
 #include "User.h"
 
+/**
+ * Constructor
+**/
 Driver::Driver(int id, int s, int d, tms dep, tms arr, int dT, int aT, unsigned cap):User(id, s, d, dep, arr, dT, aT, true), capacity(cap), currOcup(0){}
 
-void Driver::setCurrOcup(int co){
-    this->currOcup = co;
-}
+/**
+ * Set funtion
+**/
+void Driver::setCurrOcup(int co){ this->currOcup = co;}
 
-unsigned Driver::getCapacity() const{
-    return this->capacity;
-}
-
-unsigned Driver::getCurrOcup() const{
-    return this->currOcup;
-}
+/**
+ * Get funtions
+**/
+unsigned Driver::getCapacity() const{ return this->capacity;}
+unsigned Driver::getCurrOcup() const{ return this->currOcup;}
--- src/User.h
+++ src/User.h
@@ -13,11 +13,44 @@ private:
     int sourceID, destinationID;
     bool driver;
 public:
+
+    /**
+     * Constructors
+    **/
+    User(){};
     User(int id, int sourceID, int destinationID, tms dep, tms arr, int dT, int aT, bool driving);
+
+    /**
+     * Get functions
+    **/
     unsigned getId() const;
+    int getDepTol()const;
+    int getArrTol()const;
+    tms getDeparture()const;
+    tms getArrival()const;
     int getSourceID() const;
     int getDestinationID() const;
+
+    /**
+     * Set funtions
+    **/
+    void setId(int id);
+    void setDepTol(int depTol);
+    void setArrTol(int arrTol);
+    void setDeparture(tms departure);
+    void setArrival(tms arrival);
+    void setSourceID(int sourceId);
+    void setDestinationID(int destinationId);
+    
+    
+    /**
+     * Verifies if the user is the driver.
+    **/
     bool isDriver() const;
+
+    /**
+     *  
+    **/
     bool withinArrTol(tms eta);
     bool withinDepTol(tms etd);
 };
